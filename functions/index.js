c0e93b123abebe801ca050dad617b7afca5a6bb1
Caine J Nielsen <caine.nielsen@caineandrebekah.com>
2020-07-20 21:11:21 -0600

    to github!

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,32 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+var db = admin.firestore();
+
+var docRef = db.collection('users').doc('alovelace');
+
+exports.addUser = functions.https.onRequest((request, response) => {
+
+    db.collection('users').get()
+    // eslint-disable-next-line promise/always-return
+    .then((snapshot) => {
+        snapshot.forEach((doc) => {
+        console.log(doc.id, '=>', doc.data());
+        response.send(doc.id, '=>', doc.data());
+        });
+    })
+    .catch((err) => {
+        console.log('Error getting documents', err);
+        response.send('Error getting documents', err);
+    });
+
+    // var setUser = docRef.set({
+    //     first: 'Ada',
+    //     last: 'Lovelace',
+    //     born: 1816
+    // });
+});
+
+
